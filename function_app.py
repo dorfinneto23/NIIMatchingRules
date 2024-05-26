2ec8c85c042f154877f1b53404f43ee813e4a793
tech dor <tech.aharoni@gmail.com>
2024-05-26 20:11:00 +0300

    first deployment

--- function_app.py
+++ function_app.py
@@ -6,5 +6,5 @@ app = func.FunctionApp()
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="niimatchingrules",
                                connection="medicalanalysis_SERVICEBUS") 
 def NIIMatchingRules(azservicebus: func.ServiceBusMessage):
-    logging.info('Python ServiceBus Queuee trigger processed a message: %s',
+    logging.info('Python ServiceBus Queuee trigger processedd a message: %s',
                 azservicebus.get_body().decode('utf-8'))
