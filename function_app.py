201919ef1d776a0ca10fcef6ba1cf52d40e1172b
tech dor <tech.aharoni@gmail.com>
2024-05-28 01:02:26 +0300

    adding tool_resources

--- function_app.py
+++ function_app.py
@@ -66,7 +66,11 @@ def assistant_request(csv_string, assistant_id, vector_store_id):
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
         assistant_id=assistant_id,
-        tools=[{"type": "file_search"}]
+        tools=[{"type": "file_search"}],
+        tool_resources= {
+         "file_search": {
+         "vector_store_ids": [vector_store_id]}
+        }
     )
 
     # Wait for the run to complete
